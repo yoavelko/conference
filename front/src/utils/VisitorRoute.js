3a7e00122a9f2258d4bd8fe74040fade1d7e6d48
yoavelko <129358732+yoavelko@users.noreply.github.com>
2023-07-03 10:04:03 +0300

    Update VisitorRoute.js

--- front/src/utils/VisitorRoute.js
+++ front/src/utils/VisitorRoute.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.DEV ? "http://localhost:3000" : import.meta.env.SERVER_LINK;
+export const HOST = import.meta.env.DEV ? "http://localhost:3000" : import.meta.env.VITE_SERVER;
 export const createVisitor = `${HOST}/visitor/create`;
 export const getAllVisitors = `${HOST}/visitor`;
 export const statusBulkUpdate = `${HOST}/visitor/bulk-status-update`;
